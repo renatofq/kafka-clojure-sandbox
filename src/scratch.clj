b9e4ad5116ff3ab6a6517c1415c7e116b891d8fd
F8543002 Renato Fernandes De Queiroz <renatofq@bb.com.br>
2024-03-25 14:07:07 -0300

    ok-not-ok pipeline

--- src/scratch.clj
+++ src/scratch.clj
@@ -15,12 +15,6 @@
    "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
    "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"})
 
-(def producer (kclient/producer kc-config (kserdes/string-serde) (kserdes/edn-serde)))
-(force (kclient/send! producer "fetches" "user-1"
-                 {:key "jon.doe@email.com" :status 200}))
-(force (kclient/send! producer "pushes"  "user-1"
-                 {:key "jon.doe@email.com" :value 100.00}))
-
 (def ks-config
   {"application.id" "scratch"
    "bootstrap.servers" "localhost:9092"
@@ -38,45 +32,80 @@
       (kstreams/peek #(println topic ":>" %1 %2))
       (kstreams/map-values (fn [_] (get topic 0)))))
 
-(def kstreams
-  (let [builder (kstreams/make-streams-builder)
-        fetch-ticks (make-ticks-stream builder "fetches")
-        push-ticks (make-ticks-stream builder "pushes")]
+(defn make-ok-nok-pipeline
+  [builder topic-name]
+  (let [get-stream (kstreams/make-kstream builder
+                                          topic-name
+                                          (kserdes/string-serde)
+                                          (kserdes/edn-serde))]
 
-    (-> (kstreams/merge fetch-ticks push-ticks)
+    (-> get-stream
         (kstreams/group-by-key)
-        (kstreams/windowed-by 30 5)
+        (kstreams/windowed-by 90 5)
         (kstreams/aggregate
-         (fn [] {:fetches-count 0, :pushes-count 0})
+         (fn [] {:ok-count 0, :nok-count 0})
          (fn [_ v acc]
-           (if (= v \f)
-             (update acc :fetches-count inc)
-             (update acc :pushes-count inc)))
+           (case v
+             200 (update acc :ok-count inc)
+             (400 404 429 529 1529) (update acc :nok-count inc)
+             acc))
          (kserdes/string-serde)
          (kserdes/edn-serde))
         (kstreams/ktable->kstream)
         (kstreams/peek #(println "counts:" %1 %2))
-        (kstreams/filter (fn [_ v] (> (:fetches-count v) 0)))
+        (kstreams/filter (fn [_ v] (> (:nok-count v) 0)))
         (kstreams/map-values
-         (fn [v] (double (/ (:pushes-count v) (:fetches-count v)))))
+         (fn [v] (double (/ (:ok-count v) (:nok-count v)))))
         (kstreams/peek #(println "ratio:" %1 %2))
-        (kstreams/to "ratios" (kstreams/windowed-string-serde) (kserdes/double-serde)))
-
-    (kstreams/start-kafka-streams builder ks-config)))
+        (kstreams/to "ratios"
+                     (kstreams/windowed-string-serde)
+                     (kserdes/double-serde)))))
 
 (comment
   (def consumer (kclient/consumer kc-config))
+  (def producer (kclient/producer kc-config (kserdes/string-serde) (kserdes/edn-serde)))
+  (def builder (kstreams/make-streams-builder))
+  (make-ok-nok-pipeline builder "fetches")
+  (def kafka-streams (kstreams/start-kafka-streams builder ks-config))
+
   (kclient/subscribe consumer ["ratios"])
 
-  (pprint (force (kclient/send! producer "fetches"  "user-1"
-                           {:key "jon.doe@email.com" :status 200})))
-  (pprint (force (kclient/send! producer "pushes" "user-1"
-                           {:key "jon.doe@email.com" :value 100.00})))
+  (force (kclient/send! producer "fetches"  "user-1"
+                        {:key "jon.doe@email.com" :status 200}))
+  (force (kclient/send! producer "pushes" "user-1"
+                        {:key "jon.doe@email.com" :value 100.00}))
 
   (pprint (kclient/poll consumer 1))
 
   (kstreams/stop-kafka-streams kstreams)
 
   (.close consumer)
-  (.close producer)
-)
+  (.close producer))
+
+(comment
+  ;; Backup do do pipeline anterior
+  (def kstreams
+    (let [builder (kstreams/make-streams-builder)
+          fetch-ticks (make-ticks-stream builder "fetches")
+          push-ticks (make-ticks-stream builder "pushes")]
+
+      (-> (kstreams/merge fetch-ticks push-ticks)
+          (kstreams/group-by-key)
+          (kstreams/windowed-by 30 5)
+          (kstreams/aggregate
+           (fn [] {:fetches-count 0, :pushes-count 0})
+           (fn [_ v acc]
+             (if (= v \f)
+               (update acc :fetches-count inc)
+               (update acc :pushes-count inc)))
+           (kserdes/string-serde)
+           (kserdes/edn-serde))
+          (kstreams/ktable->kstream)
+          (kstreams/peek #(println "counts:" %1 %2))
+          (kstreams/filter (fn [_ v] (> (:fetches-count v) 0)))
+          (kstreams/map-values
+           (fn [v] (double (/ (:pushes-count v) (:fetches-count v)))))
+          (kstreams/peek #(println "ratio:" %1 %2))
+          (kstreams/to "ratios" (kstreams/windowed-string-serde) (kserdes/double-serde)))
+
+      (kstreams/start-kafka-streams builder ks-config))))
